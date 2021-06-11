0a9731c078609620a069af47c825e6bca2ffbfae
용국 최 <opzerg9378@gmail.com>
2021-06-11 09:13:24 +0900

    feature: add factoryMethod project in solution && wraite sample code

---
+++ FactoryMethod/PhoneFactory.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal interface Phone
+    {
+        bool PowerOff();
+        bool PowerOn();
+    }
+
+    internal abstract class PhoneFactory
+    {
+        protected abstract int MaxVersion { get; }
+        internal bool TestResult { get; private set; }
+        protected abstract Phone CreatePhone(string model, int version);
+
+        internal Phone TestPhone(string model, int version)
+        {
+            if (version > MaxVersion)
+                return null;
+
+            Phone phone = CreatePhone(model, version);
+
+            TestResult |= phone.PowerOn();
+            TestResult |= phone.PowerOff();
+
+            return phone;
+        }
+    }
+
+    internal class IPhoneFactory : PhoneFactory
+    {
+        protected override int MaxVersion => 12;
+
+        protected override Phone CreatePhone(string model, int version)
+        {
+            if (model.Equals("pro"))
+                return new IPhoenPro(version);
+            else if (model.Equals("mini"))
+                return new IPhoenMini(version);
+            else
+                return null;
+        }
+    }
+
+    internal class GalaxyFactory : PhoneFactory
+    {
+        protected override int MaxVersion => 21;
+
+        protected override Phone CreatePhone(string model, int version)
+        {
+            if (model.Equals("s"))
+                return new GalaxyS(version);
+            else if (model.Equals("note"))
+                return new GalaxyNote(version);
+            else
+                return null;
+        }
+    }
+}
---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneFactory phoneFactory = new IPhoneFactory();
+            var mini = phoneFactory.TestPhone("mini", 12);
+            var pro = phoneFactory.TestPhone("pro", 12);
+
+            GalaxyFactory galaxyFactory = new GalaxyFactory();
+            var s = galaxyFactory.TestPhone("s", 20);
+            var note = galaxyFactory.TestPhone("note", 20);
+
+            Console.Read();
+
+        }
+    }
+}
