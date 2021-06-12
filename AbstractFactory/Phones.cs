0ed665e2a3b483b3f0703995eff67c751f0def56
용국 최 <opzerg9378@gmail.com>
2021-06-12 23:35:54 +0900

    add abstractFactory project in solution & write sample code

---
+++ AbstractFactory/Phones.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Phone
+    {
+        internal OS OS { get; set; }
+        internal Pay Pay { get; set; }
+    }
+
+    /// OS, Pay구성 요소 구현
+    /// 
+
+    internal class IOS : OS
+    {
+        public void SetUp() => Console.WriteLine("IOS Setup");
+    }
+
+    internal class Android : OS
+    {
+        public void SetUp() => Console.WriteLine("Android Setup");
+    }
+
+    internal class ApplePay : Pay
+    {
+        public void Payment() => Console.WriteLine("paid by apple pay");
+    }
+
+    internal class SamsungPay : Pay
+    {
+        public void Payment() => Console.WriteLine("paid by samsung pay");
+    }
+
+
+
+}
---
+++ AbstractFactory/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneFactory phoneFactory = new PhoneFactory();
+            if(phoneFactory.CreatePhone("apple") is Phone applePhone)
+            {
+                applePhone.OS.SetUp();
+                applePhone.Pay.Payment();
+            }
+
+            if(phoneFactory.CreatePhone("samsung") is Phone samsungPhone)
+            {
+                samsungPhone.OS.SetUp();
+                samsungPhone.Pay.Payment();
+            }
+
+            Console.Read();
+        }
+    }
+}
