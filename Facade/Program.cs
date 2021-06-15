b84b8c4a2322ddbd04647aa973f368666a5db5b1
용국 최 <opzerg9378@gmail.com>
2021-06-16 08:55:14 +0900

    feature: add facade project in solution & write sample code

---
+++ Facade/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Home home = new Home();
+
+            // 집 들어가기
+            home.In();
+            Console.WriteLine("=====");
+            // fail 1. 한번 더 들어가기
+            home.In();
+            Console.WriteLine("=====");
+            // 집 나가기
+            home.Out();
+            Console.WriteLine("=====");
+            // fail 2. 한번 더 나가기
+            home.Out();
+
+            Console.ReadLine();
+        }
+    }
+}
