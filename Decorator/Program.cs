79f1d416aa721bb6dbb9aa76667a8ba0a3786433
용국 최 <opzerg9378@gmail.com>
2021-06-17 08:51:37 +0900

    feature: add decorator project in solution & write sample code

---
+++ Decorator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var visualStudio = new VisualStudio();
+            visualStudio.Setup();
+            Console.WriteLine("=======================");
+            var dotnetFramework = new DotnetFramework(visualStudio);
+            dotnetFramework.Setup();
+            Console.WriteLine("=======================");
+            var winform = new Winform(dotnetFramework);
+            winform.Setup();
+
+        }
+    }
+}
---
+++ Decorator/VS.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    internal abstract class DeveloperTool
+    {
+        internal abstract void Setup();
+    }
+    internal abstract class DeveloperToolDecorator : DeveloperTool
+    {
+        private DeveloperTool Decorator { get; }
+
+        protected DeveloperToolDecorator(DeveloperTool decorator)
+        {
+            Decorator = decorator;
+        }
+
+        internal override void Setup() => Decorator.Setup();
+    }
+    internal class VisualStudio : DeveloperTool
+    {
+        internal override void Setup() => Console.WriteLine("Visual Studio Install!!!");
+    }
+
+    internal class DotnetFramework : DeveloperToolDecorator
+    {
+        public DotnetFramework(DeveloperTool decorator) : base(decorator)
+        {
+        }
+
+        internal override void Setup()
+        {
+            base.Setup();
+            Framework45();
+        }
+
+        private void Framework45() => Console.WriteLine(".Net Framework 4.5 Install!!!");
+    }
+
+    internal class Winform : DeveloperToolDecorator
+    {
+        public Winform(DeveloperTool decorator) : base(decorator)
+        {
+        }
+
+        internal override void Setup()
+        {
+            base.Setup();
+            CSharpWinform();
+
+        }
+        private void CSharpWinform() => Console.WriteLine("C# Winform Install!!!");
+    }
+
+}
