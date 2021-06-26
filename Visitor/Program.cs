7b9b7aacdd2edbe0b4cf6fe7a31012891dc445dc
용국 최 <opzerg9378@gmail.com>
2021-06-26 23:56:33 +0900

    feature: add visitor project & write sample code

---
+++ Visitor/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Visitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPhone phone = new IPhone();
+            Galaxy galaxy = new Galaxy();
+
+            VersionVisitor versionVisitor = new VersionVisitor();
+
+            phone.Accept(versionVisitor);
+            galaxy.Accept(versionVisitor);
+
+            Console.Read();
+        }
+    }
+}
