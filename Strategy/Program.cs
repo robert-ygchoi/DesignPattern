dd5bab6a77d90a09c95ba48f19b4347e78260b51
opzerg <opzerg9378@gmail.com>
2021-06-27 23:52:44 +0900

    feature: add strategy project & write sample code

---
+++ Strategy/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // 냉장고 문의 기능들을 생성한다.
+            // 1. 냉장고 문
+            // 2. 냉동고 문
+            // 3. 김치냉장고 문
+            // ※ 테스트 코드이고 각 문 클래스 안에는 필드나 프로퍼티가 없으므로 재사용함.
+            //
+
+            FridgeDoor fridgeDoor = new FridgeDoor();
+            FreezeDoor freezeDoor = new FreezeDoor();
+            KimchiDoor kimchiDoor = new KimchiDoor();
+
+            //
+            // 1 문 두 개 냉장고를 생성한다.
+            //  1-1 위에는 냉동고, 아래는 냉장고로 만든다.
+            //  1-2 위에는 김치냉장고, 아래는 냉동고로 만든다.
+            //
+
+            TwoDoorRefrigerator twoDoorRefrigerator = new TwoDoorRefrigerator();
+            twoDoorRefrigerator.SetDoor(freezeDoor, fridgeDoor);
+            twoDoorRefrigerator.Works();
+
+            twoDoorRefrigerator.SetDoor(kimchiDoor, freezeDoor);
+            twoDoorRefrigerator.Works();
+
+            Console.WriteLine("=========");
+            //
+            // 2 문 네 개 냉장고를 생성한다.
+            //   문 생성 순서는 다음과 같다.
+            //   왼쪽 위, 오른쪽 위, 왼쪽 아래, 오른쪽 아래
+            // 2-1 냉장고, 냉장고, 냉동고, 냉동고로 만든다.
+            // 2-2 냉장고, 냉장고, 김치냉장고, 냉동고로 만든다.
+            //
+
+            FourDoorRefrigerator fourDoorRefrigerator = new FourDoorRefrigerator();
+            fourDoorRefrigerator.SetDoor(fridgeDoor, fridgeDoor, freezeDoor, freezeDoor);
+            fourDoorRefrigerator.Works();
+
+            fourDoorRefrigerator.SetDoor(fridgeDoor, fridgeDoor, kimchiDoor, freezeDoor);
+            fourDoorRefrigerator.Works();
+        }
+    }
+}
---
+++ Strategy/Refrigerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//
+// 냉장고 문의 역할을 전략패턴으로 구현
+//
+namespace Strategy
+{
+    //
+    // 냉장고 문의 행위
+    //
+    internal interface IRefrigeratorDoor
+    {
+        void Work();
+    }
+    
+    //
+    // 냉장고 부모 클래스
+    //
+    internal abstract class Refrigerator
+    {
+        protected IRefrigeratorDoor[] Doors { get; private set; }
+        protected abstract int DoorCount { get; }
+        protected abstract string GetDoorLocation(int doorIdx);
+
+        public void SetDoor(params IRefrigeratorDoor[] refrigeratorDoor)
+        {
+            if(DoorCount != refrigeratorDoor.Length)
+            {
+                Console.WriteLine("문의 갯수가 맞지 않습니다.");
+                return;
+            }
+
+            Doors = refrigeratorDoor;
+        }
+
+        public void Works()
+        {
+            if(Doors is null)
+            {
+                Console.WriteLine("문을 아직 달지 않았습니다.");
+                return;
+            }
+
+            for (int idx = 0; idx < Doors.Length; idx++)
+            {
+                Console.Write($"{GetDoorLocation(idx)}문 ");
+                Doors[idx].Work();
+            }
+        }
+    }
+
+    //
+    // 문 네 개 냉장고
+    //
+    internal class FourDoorRefrigerator : Refrigerator
+    {
+        protected override int DoorCount => 4;
+
+        protected override string GetDoorLocation(int doorIdx) => doorIdx switch { 0 => "왼쪽 윗", 1 => "오른쪽 윗", 2 => "왼쪽 아랫", 3 => "오른쪽 아랫", _ => throw new ArgumentOutOfRangeException("문은 총 네짝입니다.") };
+    }
+
+    //
+    // 문 두 개 냉장고
+    //
+    internal class TwoDoorRefrigerator : Refrigerator
+    {
+        protected override int DoorCount => 2;
+
+        protected override string GetDoorLocation(int doorIdx) => doorIdx switch { 0 => "윗", 1 => "아랫", _ => throw new ArgumentOutOfRangeException("문은 총 네짝입니다.") };
+    }
+
+    //
+    // 행위를 클래스화 한다.
+    //
+    internal class KimchiDoor : IRefrigeratorDoor
+    {
+        public void Work() => Console.WriteLine("잘익는 김치냉장고문");
+    }
+    internal class FridgeDoor : IRefrigeratorDoor
+    {
+        public void Work() => Console.WriteLine("너무 시원한 냉장고문");
+    }
+    internal class FreezeDoor : IRefrigeratorDoor
+    {
+        public void Work() => Console.WriteLine("꽁꽁 어는 냉동고문");
+    }
+}
