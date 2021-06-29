6461af26a48a8e010f4b4c4aaa8ff60a7e7c8241
용국 최 <opzerg9378@gmail.com>
2021-06-29 09:09:10 +0900

    feature: add mediator project & write sample code

---
+++ Mediator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ControlMediator controlMediator = new ControlMediator();
+
+            ListView listView = new ListView(controlMediator);
+            Button button = new Button(controlMediator);
+
+            listView.Exec();
+            Console.WriteLine("===");
+            button.Exec();
+
+            Console.ReadLine();
+        }
+    }
+}
