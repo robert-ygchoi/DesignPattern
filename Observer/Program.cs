0ed0b96b7ddf9aee626fee34c10f9a4f1961b253
용국 최 <opzerg9378@gmail.com>
2021-07-02 08:34:52 +0900

    feature: add observer project & write sample code

---
+++ Observer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+            // 1. 신세계에서 이벤트 상품을 n% 세일한다.
+            //
+
+            SinSaeGaeMall sinSaeGaeMall = new SinSaeGaeMall();
+            NIKE nike = new NIKE();
+            NBA nba = new NBA();
+
+            sinSaeGaeMall.Add(nike);
+            sinSaeGaeMall.Add(nba);
+
+            sinSaeGaeMall.Notify(30);
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Observer/Shopping.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal interface IEventItem
+    {
+        void SendMessage(int sale);
+    }
+
+    internal interface IEventHall
+    {
+        void Add(IEventItem eventItem);
+        void Remove(IEventItem eventItem);
+        void Notify(int sale);
+    }
+
+    internal class SinSaeGaeMall : IEventHall
+    {
+        private List<IEventItem> EventItems { get; set; }
+        public SinSaeGaeMall()
+        {
+            EventItems = new List<IEventItem>();
+        }
+
+        public void Add(IEventItem eventItem) => EventItems.Add(eventItem);
+
+        public void Notify(int sale) => EventItems.ForEach(eventItem => eventItem.SendMessage(sale));
+
+        public void Remove(IEventItem eventItem) => EventItems.Remove(eventItem);
+    }
+
+    internal class NIKE : IEventItem
+    {
+        public void SendMessage(int sale) => Console.WriteLine($"NIKE 전 품목 {sale}% SALE!!!");
+    }
+    internal class NBA : IEventItem
+    {
+        public void SendMessage(int sale) => Console.WriteLine($"NBA 모자 품목 {sale}% SALE!!!");
+    }
+}
